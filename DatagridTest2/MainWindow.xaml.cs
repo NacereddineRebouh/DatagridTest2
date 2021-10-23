3c858ed3352874b2b6958d88a163152e8cda2cc6
NacereddineRebouh <llbl2153@gmail.com>
2021-10-23 09:55:26 +0100

    23/10/2021

--- DatagridTest2/MainWindow.xaml.cs
+++ DatagridTest2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,47 @@ namespace Startup
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            //RadioButton Item = sender as RadioButton;
+            //double h = 20;
+            //ControlTemplate i = Item.Template;
+            //Rectangle selection_Rectangle = i.FindName("selection_Rectangle", Item) as Rectangle;
+            //var mousePoint = Mouse.GetPosition(Item);
+            //DoubleAnimation Height = null;
+            //if (Item.IsChecked == true)
+            //{
+            //    Height = new DoubleAnimation(0, h, new Duration(TimeSpan.FromSeconds(0.2)));
+            //}
+            //else
+            //{
+            //    Height = new DoubleAnimation(h, 0, new Duration(TimeSpan.FromSeconds(0.2)));
+            //}
+
+            //Storyboard sb = new Storyboard();
+            //Storyboard.SetTarget(Height, selection_Rectangle);
+            //Storyboard.SetTargetProperty(Height, new PropertyPath(HeightProperty));
+
+            //QuadraticEase qe = new QuadraticEase();
+            //qe.EasingMode = EasingMode.EaseOut;
+            //Height.EasingFunction = qe;
+
+            //sb.Children.Add(Height);
+            //sb.Begin(this);
+        }
+
+        private void uc_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Debug.WriteLine(Panel.GetZIndex(((FrameworkElement)e.Source)));
+            if (!((FrameworkElement)e.Source).Name.Equals("Sidemenu"))
+            {
+                if (Sidemenu.toggle.IsChecked == true)
+                {
+                    Sidemenu.toggle.IsChecked = false;
+                }
+            }
+
         }
     }
 }
