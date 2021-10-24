d53d0a6f13426e1f1f854fbc4ab3225849218837
NacereddineRebouh <llbl2153@gmail.com>
2021-10-24 23:27:44 +0100

    24/10/2021

--- DatagridTest2.Converters/CurrentViewConverter.cs
+++ DatagridTest2.Converters/CurrentViewConverter.cs
@@ -14,33 +14,29 @@ namespace DatagridTest2.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            //    BaseViewModel baseViewModel = (BaseViewModel)value;
-            //    Debug.WriteLine(baseViewModel.GetType() + " pp :" + parameter.ToString());
-            //    if (parameter.ToString() == "Home")
-            //    {
-            //        if (baseViewModel.GetType().Equals(new HomeViewModel().GetType()))
-            //        {
-            //            Debug.WriteLine("in");
-            //            return true;
-            //        }
-            //        else
-            //        {
-            //            return false;
-            //        }
-            //    }
-            //    else
-            //    {
-            //        if (baseViewModel.GetType().Equals(new UsersViewModel().GetType()))
-            //        {
-            //            Debug.WriteLine("in");
-            //            return true;
-            //        }
-            //        else
-            //        {
-            //            return false;
-            //        }
-            //    }
-            return true;
+            BaseViewModel baseViewModel = (BaseViewModel)value;
+            if (parameter.ToString() == "Home")
+            {
+                if (baseViewModel.GetType().Equals(new HomeViewModel().GetType()))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (baseViewModel.GetType().Equals(new UsersViewModel().GetType()))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
--- DatagridTest2.ViewModels/HomeViewModel.cs
+++ DatagridTest2.ViewModels/HomeViewModel.cs
@@ -1,98 +1,30 @@
-﻿
-
+﻿using DatagridTest2.EFramework;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace DatagridTest2.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
-        //public DatabaseAccess Dba { get; set; }
-        //private List<Users> _UsersList = null;
+        DatagridTest2Context db;
+        private List<User> _UsersList = null;
         //private List<Users> _UsersListDummy = null;
         //private Thread thread;
         //bool keepthreading = true;
-        //public List<Users> UsersList
-        //{
-        //    get { return _UsersList; }
-        //    set { _UsersList = value; OnPropertyChanged("UsersList"); }
-        //}
-
-        public HomeViewModel()
-        { }
-        //public HomeViewModel(bool LoopUpdate)
-        //{
-        //    keepthreading = LoopUpdate;
-        //    loopUpdate();
-        //}
-
-        //public void loopUpdate()
-        //{
-        //    thread = new Thread(() =>
-        //    {
-        //        Thread.CurrentThread.IsBackground = true;
-        //        threading();
-        //    });
-        //    thread.Start();
-        //    Debug.WriteLine("Threading has been Called Successfully! :" + Thread.CurrentThread.ManagedThreadId);
-
-        //}
-        //private void threading()
-        //{
-        //    Debug.WriteLine("----------Welcome to HomeViewModel threading Method------------");
-        //    //Dba = new DatabaseAccess();
-        //    //Dba.OpenConnection();
-        //    //while (keepthreading == true)
-        //    //{
-        //    //    _UsersListDummy = Dba.Get_Users();
-        //    //    UsersList = _UsersListDummy;
-        //    //    if (_UsersListDummy.Count != UsersList.Count)
-        //    //    {
-        //    //        UsersList = _UsersListDummy;
-        //    //    }
-
-        //    //    //OperationsList 
-        //    //    Debug.WriteLine("Updated!" + UsersList.Count + "  " + Thread.CurrentThread.ManagedThreadId);
-
-        //    //    Thread.Sleep(5000);
-        //    //}
-
-        //}
-
-        public void dispose()
+        public List<User> UsersList
         {
-            //thread.Join();
-            //    keepthreading = false;
+            get { return _UsersList; }
+            set { _UsersList = value; OnPropertyChanged("UsersList"); }
         }
-
-
-        //public void Quick_Update()
-        //{
-        //    new Thread(() =>
-        //    {
-        //        Thread.CurrentThread.IsBackground = true;
-        //        continueUpdate();
-        //    }).Start();
-        //}
-
-        //public void continueUpdate()
-        //{
-        //    //Debug.WriteLine("inside continueUpdate!");
-        //    //Dba = new DatabaseAccess();
-        //    //Dba.OpenConnection();
-        //    //_UsersListDummy = Dba.Get_Users();
-        //    //UsersList = _UsersListDummy;
-        //    //if (_UsersListDummy.Count != UsersList.Count)
-        //    //{
-        //    //    Debug.WriteLine("update done!");
-        //    //    UsersList = _UsersListDummy;
-        //    //}
-        //}
-
+        public HomeViewModel()
+        {
+            db = new DatagridTest2Context();
+            UsersList = db.Users.ToList();
+        }
+        public void dispose()
+        { }
     }
 }
